1a135e646af2a6d1c2da1e43ff6f6b9a730d860c
KgKyawHtin-333 <kgkyawhtin539@gmail.com>
2022-02-18 20:57:29 +0630

    first commit of realtime_breakfast_website

---
+++ js/app.js
@@ -0,0 +1,62 @@
+// Navbar menu show-hide
+$("#showMenu").on("click", function() {
+    $("#navbarNav").toggleClass("show-menu");
+});
+
+// Slick
+$('.slick-img').slick({
+    arrows: false,
+    dots: true,
+    vertical: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+});
+
+// Wow js
+wow = new WOW(
+    {
+    boxClass:     'wow',      // default
+    animateClass: 'animate__animated', // default
+    offset:       0,          // default
+    mobile:       true,       // default
+    live:         true        // default
+  }
+  )
+  wow.init();
+
+// For Loading Screen
+$(window).on("load", function() {
+    $(".loader-container").fadeOut(300, function() {
+        $(this).remove();
+    })
+})
